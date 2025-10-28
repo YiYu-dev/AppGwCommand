e2f87af84d88833df4ac2adb4a3fc8ccf7353e38
YiYu-dev <yuta.contact03@gmail.com>
2025-10-28 18:26:48 +0900

    Add files via upload

---
+++ appgw_rt_config.ps1
@@ -0,0 +1,250 @@
+az login
+Connect-AzAccount
+$location = "japan east"
+$SubscriptionId = (az account show | convertfrom-json).id
+$tenantId = (az account show | convertfrom-json).tenantId
+$Id = "002"
+$agwId = "02"
+$ResourceDefaultNm = "jp.rbs.wak-any"
+$ResourceDefaultNmhyphen = "jp-rbs-wak-any"
+$ResourceBaseNm = "$ResourceDefaultNm-$Id"
+$ResourceBaseNmhyphen = "$ResourceDefaultNmhyphen-$Id"
+$RgNm = "$ResourceBaseNm-rg"
+$RgCommonNm = "$ResourceDefaultNm-rg"
+$VNetNm = "$ResourceDefaultNm-vnet-spoke"
+$OperationUser = (az account show | convertfrom-json).user.name
+$KeyVaultCommonNm = "$ResourceDefaultNmhyphen-kv2"
+$agwNm = "$ResourceDefaultNm-agw$agwId"
+$agwWafpolicy = "$ResourceDefaultNm-wafpolicy02"
+$agwbackendpooldefault = "$ResourceDefaultNm-default-backend"
+$agwbackendpoolwa = "$ResourceDefaultNm-$Id-wa-backend"
+$agwbackendpoolfa = "$ResourceDefaultNm-$Id-fa-backend"
+$agwbackendsettingdefault = "$ResourceDefaultNm-default-backendSetting"
+$agwbackendsettingwa = "$ResourceDefaultNm-$Id-wa-backendSetting"
+$agwbackendsettingfa = "$ResourceDefaultNm-$Id-fa-backendSetting"
+$agwbackenddefaulttarget = "$ResourceDefaultNm-default-target"
+$agwbackendtargetwa = "$ResourceDefaultNm-$Id-wa-backendtarget"
+$agwbackendtargetfa = "$ResourceDefaultNm-$Id-fa-backendtarget"
+$agwAffinitywa = "AGA${Id}wa"
+$agwAffinityfa = "AGA${Id}fa"
+$agwhelthprobewa = "$ResourceDefaultNm-$Id-wa-health"
+$agwhelthprobefa = "$ResourceDefaultNm-$Id-fa-health"
+$agwListner = "$ResourceDefaultNm-Listner"
+$agwRouteRule = "$ResourceDefaultNm-RouteRule"
+$agwWildCard = "*.rbsdevtest2.com"
+$agwFuncAppRewrite = "FuncAppCommonRules"
+$agwWebAppRewrite = "WebAppCommonRules"
+$agwDefaultNm = "snet-agw-biz"
+$agwSubnetNm = "$agwDefaultNm-$agwId"
+$agwSubnetNmCIDR = "172.16.16.0/24"
+$agw = "$ResourceDefaultNm-agw$agwId"
+$agwpip = "$ResourceDefaultNm-agw$agwId-pip"
+$agwIPConfig = "appGatewayIpConfig-$agwId"
+$agwFIPConfig = "appGwPublicFrontendIpIPv4-$agwId"
+$agwcertname = (az keyvault certificate list --vault-name $KeyVaultCommonNm |ConvertFrom-Json).name
+$Wa = "$ResourceDefaultNmhyphen-$Id-wa02"
+$Fa = "$ResourceDefaultNmhyphen-$Id-fa02"
+$backendPoolwa =  (az network application-gateway address-pool create `
+--resouce-group $RgCommonNm `
+--gateway-name $agwNm `
+--name $agwbackendpoolwa `
+--servers "$Wa.azurewebsites.net" '
+| ConvertFrom-Json)
+$probewa = (az network application-gateway probe create `
+--g $RgCommonNm `
+--gateway-name $agwNm `
+-n $agwhealthprobewa `
+--protocol https `
+--host "$Wa.azurewebsites.net" `
+--path "/${Id}/admin/.auth/login/aad/callback" `
+--interval 30 `
+--timeout 30 `
+--threshold 3 `
+--min-servers 0 `
+| ConvertFrom-Json).Id
+$httpSettingwa = (az network application-gateway http-settings create `
+-g $RgCommonNm `
+--gateway-name $agwNm `
+-n $agwbackendsettingwa `
+--port 443 `
+--protocol Https `
+--cookie-based-affinity Enabled `
+--affinity-cookie-name "AGA${Id}wa" `
+--timeout 20 `
+--host-name-from-backend-pool 1 `
+--probe $probewa `
+| ConvertFrom-Json).Id
+$UrlPathMapId = az network application-gateway rule show `
+--gateway-name $agwNm `
+--resource-group $RgCommonNm `
+--name $agwRouteRule `
+--query "urlPathMap.id"
+$PathRuleWa = (az network application-gateway url-path-map rule create `
+--gateway-name $agwNm `
+--resource-group $RgCommonNm `
+--path-map-name $agwRouteRule `
+--name $agwbackendtargetwa `
+--paths "/${Id}/admin/*" `
+--address-pool $agwbackendpoolwa `
+--http-settings $httpSettingwa `
+| ConvertFrom-Json)
+$backendPoolwa =  (az network application-gateway address-pool create `
+--resouce-group $RgCommonNm `
+--gateway-name $agwNm `
+--name $agwbackendpoolfa `
+--servers "$Fa.azurewebsites.net" '
+| ConvertFrom-Json)
+$probewa = (az network application-gateway probe create `
+--g $RgCommonNm `
+--gateway-name $agwNm `
+-n $agwhealthprobefa `
+--protocol https `
+--host "$Fa.azurewebsites.net" `
+--path "/${Id}/api/.auth/login/aad/callback" `
+--interval 30 `
+--timeout 30 `
+--threshold 3 `
+--min-servers 0 `
+| ConvertFrom-Json).Id
+$httpSettingwa = (az network application-gateway http-settings create `
+-g $RgCommonNm `
+--gateway-name $agwNm `
+-n $agwbackendsettingfa `
+--port 443 `
+--protocol Https `
+--cookie-based-affinity Enabled `
+--affinity-cookie-name "AGA${Id}fa" `
+--timeout 220 `
+--host-name-from-backend-pool 1 `
+--probe $probefa `
+| ConvertFrom-Json).Id
+$UrlPathMapId = az network application-gateway rule show `
+--gateway-name $agwNm `
+--resource-group $RgCommonNm `
+--name $agwRouteRule `
+--query "urlPathMap.id"
+$PathRuleFa = (az network application-gateway url-path-map rule create `
+--gateway-name $agwNm `
+--resource-group $RgCommonNm `
+--path-map-name $agwRouteRule `
+--name $agwbackendtargetfa `
+--paths "/${Id}/api/*" `
+--address-pool $agwbackendpoolfa `
+--http-settings $httpSettingfa `
+| ConvertFrom-Json)
+$appGw = Get-ApplicationGateway -Name $agwNm -RgCommonNm
+$AzUrlPathMap = appGw.UrlPathMaps | Where-Object { $_.Name -eq $agwRouteRule }
+$AzPathRuleWa = $AzUrlPathMap.PathRules | Where-Object { $_.Name -eq $agwbackendtargetwa }
+$rewriteRuleSetNameWa = "WebAppCommonRules"
+$rewriteRuleSet = $appGw.RewriteRuleSets | Where-Object { $_.Name -eq $rewriteRuleSetNameWa }
+$AzPathRuleWa.RewriteRuleSet = $rewriteRuleSet
+Set-AzApplicationGateway -ApplicationGateway $appGw
+$AzPathRuleFa = $AzUrlPathMap.PathRules | Where-Object { $_.Name -eq $agwbackendtargetfa }
+$rewriteRuleSetNameFa = "FunctionAppCommonRules"
+$rewriteRuleSet = $appGw.RewriteRuleSets | Where-Object { $_.Name -eq $rewriteRuleSetNameFa }
+$AzPathRuleFa.RewriteRuleSet = $rewriteRuleSet
+Set-AzApplicationGateway -ApplicationGateway $appGw
+az network route-table route-create `
+--address-prefix $WaPrivateIp/32
+--name "$ResouceDefaultNm-rt-to-$Id-wa01" `
+--next-hop-ip-address 192.168.100.4 `
+--next-hop-type VirtualAppliance `
+--resource-group $RgCommonNm `
+--route-table-name $RtSpoke2hub
+az network route-table route-create `
+--address-prefix $FaPrivateIp/32
+--name "$ResouceDefaultNm-rt-to-$Id-fa01" `
+--next-hop-ip-address 192.168.100.4 `
+--next-hop-type VirtualAppliance `
+--resource-group $RgCommonNm `
+--route-table-name $RtSpoke2hub
+$WaPep = az network private-endpoint show `
+    --name "$Wa-pep" `
+    --resource-group $RgCommonNm | ConvertFrom-Json
+$WaPrivateIp = $WaPep.customDnsConfigs[0].ipAddresses[0]
+az network route-table route create `
+  --address-prefix $WaPrivateIp/32 `
+  --name "$ResourceDefaultNm-rt-to-$Id-wa02" `
+  --next-hop-ip-address 192.168.100.4 `
+  --next-hop-type VirtualAppliance `
+  --resource-group $RgCommonNm `
+  --route-table-name $RtSpoke2hub
+$FaPep = az network private-endpoint show `
+    --name "$Fa-pep" `
+    --resource-group $RgCommonNm | ConvertFrom-Json
+$FaPrivateIp = $FaPep.customDnsConfigs[0].ipAddresses[0]
+az network route-table route create `
+  --address-prefix $FaPrivateIp/32 `
+  --name "$ResourceDefaultNm-rt-to-$Id-fa02" `
+  --next-hop-ip-address 192.168.100.4 `
+  --next-hop-type VirtualAppliance `
+  --resource-group $RgCommonNm `
+  --route-table-name $RtSpoke2hub
+az network vnet subnet update `
+  --resource-group $RgCommonNm `
+  --vnet-name $VNetNm `
+  --name $agwSubnetNm `
+  --route-table $RtSpoke2hub
+az network route-table create `
+  --resource-group $RgCommonNm `
+  --name $RtVnet2hub `
+  --location $location
+az network route-table route create `
+  --address-prefix 172.16.16.0/24 `
+  --name "$ResourceDefaultNm-rt-to-agw$agwId" `
+  --next-hop-ip-address 192.168.100.4 `
+  --next-hop-type VirtualAppliance `
+  --resource-group $RgCommonNm `
+  --route-table-name $RtVnet2hub
+az network route-table route create `
+  --address-prefix 0.0.0.0/0 `
+  --name "$ResourceDefaultNm-rt-toInternet" `
+  --next-hop-ip-address 192.168.100.4 `
+  --next-hop-type VirtualAppliance `
+  --resource-group $RgCommonNm `
+  --route-table-name $RtVnet2hub
+az network vnet subnet update `
+  --resource-group $RgCommonNm `
+  --vnet-name $VNetNm `
+  --name $SubnetIntegNm `
+  --route-table $RtVnet2hub
+az monitor diagnostic-settings create `
+  --name $WaDiag `
+  --resource $(az webapp show --name $Wa --resource-group $RgNm --query id -o tsv) `
+  --workspace $workspaceid `
+  --logs '[{"category":"AppServiceHTTPLogs","enabled":true},{"category":"AppServiceConsoleLogs","enabled":true},{"category":"AppServiceAppLogs","enabled":true},{"category":"AppServiceAuditLogs","enabled":true},{"category":"AppServiceIPSecAuditLogs","enabled":true},{"category":"AppServicePlatformLogs","enabled":true},{"category":"AppServiceAuthenticationLogs","enabled":true},{"category":"AppServiceAntivirusScanAuditLogs","enabled":true},{"category":"AppServiceFileAuditLogs","enabled":true}]' `
+  --metrics '[{"category":"AllMetrics","enabled":true}]'
+az monitor diagnostic-settings create `
+  --name $FaDiag `
+  --resource $(az webapp show --name $Fa --resource-group $RgNm --query id -o tsv) `
+  --workspace $workspaceid `
+  --logs '[{"category":"FunctionAppLogs","enabled":true},{"category":"AppServiceAuthenticationLogs","enabled":true}]' `
+  --metrics '[{"category":"AllMetrics","enabled":true}]'
+az monitor diagnostic-settings create `
+  --name $AppPlanDiag `
+  --resource $(az appservice plan show --name $AppPlan --resource-group $RgNm --query id -o tsv) `
+  --workspace $workspaceid `
+  --metrics '[{"category":"AllMetrics","enabled":true}]'
+az monitor diagnostic-settings create `
+  --name $KeyVaultNmDiag `
+  --resource $(az keyvault show --name $KeyVaultNm --resource-group $RgNm --query id -o tsv) `
+  --workspace $workspaceid `
+  --logs '[{"categoryGroup":"audit","enabled":true},{"categoryGroup":"allLogs","enabled":true}]' `
+  --metrics '[{"category":"AllMetrics","enabled":true}]'
+az monitor diagnostic-settings create `
+  --name $agwNmDiag `
+  --resource $(az network application-gateway show -g $RgCommonNm -n $agwNm --query id -o tsv) `
+  --workspace $workspaceid `
+  --logs '[{"categoryGroup":"allLogs","enabled":true}]' `
+  --metrics '[{"category":"AllMetrics","enabled":true}]'
+az monitor diagnostic-settings create `
+  --name $SQLDbDiag `
+  --resource $(az sql db show -g $RgNm -n $SQLDb --server $SQLSrv --query id -o tsv) `
+  --workspace $workspaceid `
+  --logs '[{"categoryGroup":"allLogs","enabled":true},{"categoryGroup":"audit","enabled":true}]' `
+  --metrics '[{"category":"Basic","enabled":true},{"category":"InstanceAndAppAdvanced","enabled":true},{"category":"WorkloadManagement","enabled":true}]'
+az monitor diagnostic-settings create `
+  --name $FunctionSADiag `
+  --resource $(az storage account show -g $RgNm -n $FunctionSANm --query id -o tsv) `
+  --workspace $workspaceid `
+  --metrics '[{"category":"Transaction","enabled":true}]'
